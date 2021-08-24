c41e846574a55018af82b7b22d47a9c18383f1ab
Khaidem Sandip Singha <37861577+Kurosakicoder@users.noreply.github.com>
2021-08-24 11:07:30 +0530

    Update views.py

--- news/newsapp/views.py
+++ news/newsapp/views.py
@@ -6,7 +6,7 @@ def home(request):
     import requests
     import json
 
-    news_api_request = requests.get("https://newsapi.org/v2/top-headlines?country=us&apiKey=759a41ef0f944f91a5939ea831376dd8")
+    news_api_request = requests.get("https://newsapi.org/v2/top-headlines?country=us&apiKey=${YOUR_API_KEY}")
     api = json.loads(news_api_request.content)
 
     return render(request, 'home.html', {'api': api})
@@ -19,7 +19,7 @@ def search(request):
         quote = quote.lower()
         
         
-        search_topic_request =  requests.get("https://newsapi.org/v2/everything?q="+ quote +"&sortBy=publishedAt&apiKey=759a41ef0f944f91a5939ea831376dd8")   
+        search_topic_request =  requests.get("https://newsapi.org/v2/everything?q="+ quote +"&sortBy=publishedAt&apiKey=${YOUR_API_KEY}")   
         search_topic = json.loads(search_topic_request.content)
         return render(request, 'search.html', {'quote': quote, 'search_topic': search_topic})
 
@@ -32,7 +32,7 @@ def sources(request):
     import requests
     import json
 
-    news_api_request = requests.get("https://newsapi.org/v2/sources?apiKey=759a41ef0f944f91a5939ea831376dd8")
+    news_api_request = requests.get("https://newsapi.org/v2/sources?apiKey=${YOUR_API_KEY}")
     api = json.loads(news_api_request.content)
 
     return render(request, 'sources.html', {'api': api})         
